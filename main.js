be6bdb00192011e8c044aad69cff5575bf9819c9
Jake Hallock <J_hallock@comcast.net>
2019-04-26 19:14:50 -0700

    Update main.js
    Initial creep spawn code

--- main.js
+++ main.js
@@ -2,6 +2,14 @@
 module.exports.loop = function() {
 	for(var name in Memory.creeps) {
 		var creep = Game.creeps[name];
-		console.log(creep);
+		if (creep == undefined) {
+			/* It's dead Jim */
+			var err = Game.Spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], name);
+			if (err != OK) {
+				console.log(err);
+			}
+		} else {
+			console.log("success");
+		}
 	}
 }
