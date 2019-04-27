896dd4f5136ae710d5a3f484cf0a84d9f696779d
Jake Hallock <J_hallock@comcast.net>
2019-04-26 20:08:47 -0700

    Update role.miner.js
    Add harvest logic

--- role.miner.js
+++ role.miner.js
@@ -1,12 +1,6 @@
 
 module.exports = {
 	run: function(creep) {
-		if (creep.memory.workroom == undefined) {
-			/* Dont have a workroom, set it */
-			creep.memory.workroom = creep.room.name;
-		}
-		var room = Game.rooms[creep.memory.workroom];
-
 		if (creep.memory.worksource == undefined) {
 			/* Dont have a worksource, set it */
 			var sources = creep.room.find(FIND_SOURCES);
@@ -15,11 +9,14 @@ module.exports = {
 		var worksource = Game.getObjectById(creep.memory.worksource);
 
 		var range = creep.pos.getRangeTo(worksource);
-		console.log(range);
 		if (range > 1) {
 			creep.moveTo(worksource);
-		} else {
-			console.log("I'm here!");
+			return;
+		}
+
+		if (creep.carry[RESOURCE_ENERGY] != creep.carryCapacity) {
+			creep.harvest(worksource);
+			return;
 		}
 	}
 };
