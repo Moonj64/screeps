a7ad8e94d0f1eefe5463b3f82bac0a4ea8b98ee9
Jake Hallock <J_hallock@comcast.net>
2019-04-27 10:57:22 -0700

    Build target selection for builders

--- role.builder.js
+++ role.builder.js
@@ -1,3 +1,4 @@
+var roleUpgrader = require('role.upgrader');
 
 module.exports = {
 	run: function(creep) {
@@ -7,6 +8,36 @@ module.exports = {
 		} else if (!creep.memory.working && creep.carry[RESOURCE_ENERGY] == creep.carryCapacity) {
 			/* Energy refilled, proceed to upgrade */
 			creep.memory.working = true;
+		}
+
+		if (creep.memory.working) {
+			var build_target;
+			if (creep.memory.build_target == undefined || 
+				((build_target = Game.getObjectById(creep.memory.build_target)) == undefined)) {
+				/* Either no defined build target or our build target doesnt exist 
+				 * In either case, get a new build target
+				 */
+				var construction_sites = creep.room.find(FIND_MY_CONSTRUCTION_SITES);
+				if (construction_sites.length == 0) {
+					/* No sites to build, act as an upgrader */
+					roleUpgrader.run(creep);
+					return;
+				}
+				build_target = construction_sites[0];
+				creep.memory.build_target = build_target.id;
+			}
+
+			
+		} else {
+			if (creep.memory.energy_source != undefined) {
+				var energy_source = Game.getObjectById(creep.memory.energy_source);
+				var	err = creep.withdraw(energy_source, RESOURCE_ENERGY);
+				if (err == ERR_NOT_IN_RANGE) {
+					creep.moveTo(energy_source);
+				}
+			} else {
+				/* TODO logic to acquire an energy source*/
+			}
 		}
 	}
 }
