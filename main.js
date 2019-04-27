1e3b44f4087caed5e15009d2aa255ff403eb8964
Jake Hallock <J_hallock@comcast.net>
2019-04-26 21:39:18 -0700

    Update main.js

--- main.js
+++ main.js
@@ -5,16 +5,16 @@ module.exports.loop = function() {
 	for(var name in Memory.creeps) {
 		var creep = Game.creeps[name];
 		if (name == "Miner2") {
-			return;
+			continue;
 		}
 		if (name == "miner2" && creep == undefined) {
 			creep = Game.creeps["Miner2"];
 		}
 		if (creep == undefined) {
 			/* It's dead Jim */
-			var err = Game.spawns["Spawn1"].spawnCreep([WORK, CARRY, MOVE], name);
+			var err = Game.spawns["Spawn1"].spawnCreep([WORK, WORK, CARRY, MOVE], name);
 			if (err != OK) {
-				console.log("Could not spawn creep: " + err);
+				console.log("Could not spawn creep (" + name + "), error code: " + err);
 			}
 		} else {
 			switch (creep.memory.role) {
