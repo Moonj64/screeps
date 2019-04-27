f946001e94431aef3f74d82e68781ce9c60205d3
Jake Hallock <J_hallock@comcast.net>
2019-04-26 19:53:25 -0700

    Update role.miner.js
    Add initial miner logic

--- role.miner.js
+++ role.miner.js
@@ -1,6 +1,19 @@
 
 module.exports = {
 	run: function(creep) {
-		
+		if (creep.memory.workroom == undefined) {
+			/* Dont have a workroom, set it */
+			creep.memory.workroom = creep.room.name;
+		}
+		var room = Game.rooms[creep.memory.workroom];
+
+		if (creep.memory.worksource == undefined) {
+			/* Dont have a worksource, set it */
+			var sources = creep.room.find(FIND_SOURCES);
+			creep.memory.worksource = sources[0].id;
+		}
+		var worksource = Game.getObjectById(creep.memory.worksource);
+
+		console.log(creep.pos.getRangeTo(worksource));
 	}
 };
